53d14db9e3ae824905ec8d88ecb4b56e100d1442
Peter Mitchell <peterjmit@gmail.com>
2013-04-16 08:48:08 -0400

    Added deployment

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+set :application, "peterjmit"
+set :domain,      "#{application}.com"
+set :deploy_to,   "/var/www/#{domain}"
+set :app_path,    "app"
+
+role :web,        domain
+role :app,        domain
+role :db,         domain, :primary => true
+
+set :repository,  "git@bitbucket.org:peterjmit/peterjmit.com.git"
+set :scm,         :git
+set :deploy_via,  :remote_cache
+set :branch,    "develop"
+
+set :user, "pete"
+set :use_sudo, false
+set :keep_releases,  3
+
+set :copy_exclude, ["sass"]
+
+after "deploy:restart", "deploy:cleanup"
